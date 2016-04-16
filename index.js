5eaef2f8e8f758251f05581a8ab8475793ce937d
Ruchira Wageesha <ruchira.wageesha@gmail.com>
2016-04-17 00:52:20 +0530

    sso changes

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 var log = require('logger')('token-service');
+var request = require('request');
+var async = require('async');
+var url = require('url');
+var agent = require('hub-agent');
 var permission = require('permission');
 var User = require('user');
 var Client = require('client');
@@ -11,11 +15,41 @@ module.exports = router;
 
 var MIN_ACCESSIBILITY = 20 * 1000;
 
-var sendToken = function (req, res, client, user) {
-    Token.findOne({
-        user: user.id,
-        client: client.id
-    }, function (err, token) {
+var REDIRECT_URI = 'https://accounts.serandives.com/auth/oauth';
+
+var context = {
+    serandives: {},
+    facebook: {
+        token: 'https://graph.facebook.com/v2.3/oauth/access_token',
+        profile: 'https://graph.facebook.com/me'
+    }
+};
+
+Client.findOne({
+    name: 'serandives'
+}, function (err, client) {
+    if (err) {
+        throw err;
+    }
+    if (!client) {
+        throw new Error('no serandives client found in the database')
+    }
+    var serandives = context.serandives;
+    serandives.id = client.id
+    serandives.secret = client.secret
+})
+
+agent.config('facebook', function (value) {
+    var app = value.app;
+    var facebook = context.facebook;
+    facebook.id = app.id;
+    facebook.secret = app.secret;
+});
+
+var sendToken = function (clientId, res, user) {
+    Client.findOne({
+        _id: clientId
+    }, function (err, client) {
         if (err) {
             log.error(err);
             res.status(500).send({
@@ -23,20 +57,13 @@ var sendToken = function (req, res, client, user) {
             });
             return;
         }
-        var expires;
-        if (token) {
-            expires = token.accessibility();
-            if (expires > MIN_ACCESSIBILITY) {
-                res.send({
-                    access_token: token.access,
-                    refresh_token: token.refresh,
-                    expires_in: expires
-                });
-                return;
-            }
+        if (!client) {
+            res.status(400).send({
+                error: 'client id not found'
+            });
+            return;
         }
-
-        Token.create({
+        Token.findOne({
             user: user.id,
             client: client.id
         }, function (err, token) {
@@ -47,11 +74,22 @@ var sendToken = function (req, res, client, user) {
                 });
                 return;
             }
-            User.update({
-                _id: user.id
-            }, {
-                token: token
-            }, function (err, user) {
+            var expires;
+            if (token) {
+                expires = token.accessibility();
+                if (expires > MIN_ACCESSIBILITY) {
+                    res.send({
+                        access_token: token.access,
+                        refresh_token: token.refresh,
+                        expires_in: expires
+                    });
+                    return;
+                }
+            }
+            Token.create({
+                user: user.id,
+                client: client.id
+            }, function (err, token) {
                 if (err) {
                     log.error(err);
                     res.status(500).send({
@@ -59,11 +97,24 @@ var sendToken = function (req, res, client, user) {
                     });
                     return;
                 }
-                res.send({
-                    id: token.id,
-                    access_token: token.access,
-                    refresh_token: token.refresh,
-                    expires_in: token.accessible
+                User.update({
+                    _id: user.id
+                }, {
+                    token: token
+                }, function (err, user) {
+                    if (err) {
+                        log.error(err);
+                        res.status(500).send({
+                            error: 'internal server error'
+                        });
+                        return;
+                    }
+                    res.send({
+                        id: token.id,
+                        access_token: token.access,
+                        refresh_token: token.refresh,
+                        expires_in: token.accessible
+                    });
                 });
             });
         });
@@ -101,24 +152,7 @@ var passwordGrant = function (req, res) {
                 });
                 return;
             }
-            Client.findOne({
-                _id: req.body.client_id
-            }, function (err, client) {
-                if (err) {
-                    log.error(err);
-                    res.status(500).send({
-                        error: 'internal server error'
-                    });
-                    return;
-                }
-                if (!client) {
-                    res.status(400).send({
-                        error: 'client id not found'
-                    });
-                    return;
-                }
-                sendToken(req, res, client, user);
-            });
+            sendToken(req.body.client_id, res, user);
         });
     });
 };
@@ -192,6 +226,93 @@ var refreshGrant = function (req, res) {
         });
 };
 
+var facebookGrant = function (req, res) {
+    var serandives = context.serandives;
+    var facebook = context.facebook;
+    request({
+        method: 'GET',
+        uri: facebook.token,
+        qs: {
+            code: req.body.code,
+            client_id: facebook.id,
+            client_secret: facebook.secret,
+            redirect_uri: REDIRECT_URI
+        },
+        json: true
+    }, function (err, response, body) {
+        if (err) {
+            res.status(401).send({
+                error: 'user not authorized'
+            });
+            return;
+        }
+        if (response.statusCode !== 200) {
+            res.status(401).send({
+                error: 'user not authorized'
+            });
+            return;
+        }
+        var access = body.access_token;
+        request({
+            method: 'GET',
+            uri: facebook.profile,
+            qs: {
+                access_token: access,
+                fields: 'email,first_name,last_name'
+            },
+            json: true
+        }, function (err, response, body) {
+            if (err) {
+                res.status(401).send({
+                    error: 'user not authorized'
+                });
+                return;
+            }
+            if (response.statusCode !== 200) {
+                res.status(401).send({
+                    error: 'user not authorized'
+                });
+                return;
+            }
+            var email = body.email;
+            if (!email) {
+                res.status(500).send({
+                    error: 'internal server error'
+                });
+                return;
+            }
+            User.findOne({
+                email: email
+            }).populate('tokens').exec(function (err, user) {
+                if (err) {
+                    log.error(err);
+                    res.status(500).send({
+                        error: 'internal server error'
+                    });
+                    return;
+                }
+                if (user) {
+                    return sendToken(serandives.id, res, user);
+                }
+                User.create({
+                    email: email,
+                    firstname: body.first_name || '',
+                    lastname: body.last_name || ''
+                }, function (err, user) {
+                    if (err) {
+                        log.error(err);
+                        res.status(500).send({
+                            error: 'internal server error'
+                        });
+                        return;
+                    }
+                    sendToken(serandives.id, res, user);
+                });
+            });
+        })
+    });
+};
+
 router.get('/tokens/:id', function (req, res) {
     var token = req.token;
     if (!token) {
@@ -206,7 +327,18 @@ router.get('/tokens/:id', function (req, res) {
         });
         return;
     }
-    res.send(permission.merge(token.has, token.client.has, token.user.has));
+    token.has = permission.merge(token.has, token.client.has, token.user.has);
+    res.send({
+        id: token.id,
+        user: token.user.id,
+        client: token.client.id,
+        access: token.access,
+        refresh: token.refresh,
+        created: token.created,
+        accessible: token.accessible,
+        refreshable: token.refreshable,
+        has: token.has
+    });
 });
 
 /**
@@ -221,6 +353,9 @@ router.post('/tokens', function (req, res) {
         case 'refresh_token':
             refreshGrant(req, res);
             break;
+        case 'facebook':
+            facebookGrant(req, res);
+            break;
         default :
             res.status(400).send({
                 error: 'unsupported grant type'
@@ -231,8 +366,8 @@ router.post('/tokens', function (req, res) {
 router.delete('/tokens/:id', function (req, res) {
     var token = req.params.id;
     Token.findOne({
-        access: token
-    })
+            access: token
+        })
         .exec(function (err, token) {
             if (err) {
                 res.status(500).send({
