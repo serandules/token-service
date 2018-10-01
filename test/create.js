d7700ee6dcb19ecb1ffd8775937fcea14c3e020b
Ruchira Wageesha <ruchira.wageesha@gmail.com>
2018-10-01 10:33:07 +0530

    updating

--- test/create.js
+++ test/create.js
@@ -5,226 +5,243 @@ var request = require('request');
 var pot = require('pot');
 
 describe('POST /tokens', function () {
-    var client;
-    before(function (done) {
-        pot.client(function (err, c) {
-            if (err) return done(err);
-            client = c;
-            done();
-        });
+  var client;
+  before(function (done) {
+    pot.client(function (err, c) {
+      if (err) return done(err);
+      client = c;
+      done();
     });
+  });
 
-    it('with no media type', function (done) {
-        request({
-            uri: pot.resolve('accounts', '/apis/v/tokens'),
-            method: 'POST',
-            json: true,
-            auth: {
-                bearer: client.users[0].token
-            }
-        }, function (e, r, b) {
-            if (e) {
-                return done(e);
-            }
-            r.statusCode.should.equal(errors.unsupportedMedia().status);
-            should.exist(b);
-            should.exist(b.code);
-            should.exist(b.message);
-            b.code.should.equal(errors.unsupportedMedia().data.code);
-            done();
-        });
+  it('with no media type', function (done) {
+    request({
+      uri: pot.resolve('accounts', '/apis/v/tokens'),
+      method: 'POST',
+      json: true,
+      auth: {
+        bearer: client.users[0].token
+      }
+    }, function (e, r, b) {
+      if (e) {
+        return done(e);
+      }
+      r.statusCode.should.equal(errors.unsupportedMedia().status);
+      should.exist(b);
+      should.exist(b.code);
+      should.exist(b.message);
+      b.code.should.equal(errors.unsupportedMedia().data.code);
+      done();
     });
+  });
 
-    it('with unsupported media type', function (done) {
-        request({
-            uri: pot.resolve('accounts', '/apis/v/tokens'),
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/xml'
-            },
-            json: true,
-            auth: {
-                bearer: client.users[0].token
-            }
-        }, function (e, r, b) {
-            if (e) {
-                return done(e);
-            }
-            r.statusCode.should.equal(errors.unsupportedMedia().status);
-            should.exist(b);
-            should.exist(b.code);
-            should.exist(b.message);
-            b.code.should.equal(errors.unsupportedMedia().data.code);
-            done();
-        });
+  it('with unsupported media type', function (done) {
+    request({
+      uri: pot.resolve('accounts', '/apis/v/tokens'),
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/xml'
+      },
+      json: true,
+      auth: {
+        bearer: client.users[0].token
+      }
+    }, function (e, r, b) {
+      if (e) {
+        return done(e);
+      }
+      r.statusCode.should.equal(errors.unsupportedMedia().status);
+      should.exist(b);
+      should.exist(b.code);
+      should.exist(b.message);
+      b.code.should.equal(errors.unsupportedMedia().data.code);
+      done();
     });
+  });
 
-    it('with unsupported grant type', function (done) {
-        request({
-            uri: pot.resolve('accounts', '/apis/v/tokens'),
-            method: 'POST',
-            form: {
-                grant_type: 'custom'
-            },
-            json: true,
-            auth: {
-                bearer: client.users[0].token
-            }
-        }, function (e, r, b) {
-            if (e) {
-                return done(e);
-            }
-            r.statusCode.should.equal(errors.unprocessableEntity().status);
-            should.exist(b);
-            should.exist(b.code);
-            should.exist(b.message);
-            b.code.should.equal(errors.unprocessableEntity().data.code);
-            done();
-        });
+  it('with unsupported grant type', function (done) {
+    request({
+      uri: pot.resolve('accounts', '/apis/v/tokens'),
+      method: 'POST',
+      form: {
+        grant_type: 'custom'
+      },
+      json: true,
+      auth: {
+        bearer: client.users[0].token
+      }
+    }, function (e, r, b) {
+      if (e) {
+        return done(e);
+      }
+      r.statusCode.should.equal(errors.unprocessableEntity().status);
+      should.exist(b);
+      should.exist(b.code);
+      should.exist(b.message);
+      b.code.should.equal(errors.unprocessableEntity().data.code);
+      done();
     });
+  });
 
-    it('password grand type without username', function (done) {
-        request({
-            uri: pot.resolve('accounts', '/apis/v/tokens'),
-            method: 'POST',
-            form: {
-                grant_type: 'password'
-            },
-            json: true,
-            auth: {
-                bearer: client.users[0].token
-            }
-        }, function (e, r, b) {
-            if (e) {
-                return done(e);
-            }
-            r.statusCode.should.equal(errors.unprocessableEntity().status);
-            should.exist(b);
-            should.exist(b.code);
-            should.exist(b.message);
-            b.code.should.equal(errors.unprocessableEntity().data.code);
-            done();
-        });
+  it('password grand type without username', function (done) {
+    request({
+      uri: pot.resolve('accounts', '/apis/v/tokens'),
+      method: 'POST',
+      headers: {
+        'X-Captcha': 'dummy'
+      },
+      form: {
+        grant_type: 'password'
+      },
+      json: true,
+      auth: {
+        bearer: client.users[0].token
+      }
+    }, function (e, r, b) {
+      if (e) {
+        return done(e);
+      }
+      r.statusCode.should.equal(errors.unprocessableEntity().status);
+      should.exist(b);
+      should.exist(b.code);
+      should.exist(b.message);
+      b.code.should.equal(errors.unprocessableEntity().data.code);
+      done();
     });
+  });
 
-    it('password grand type without password', function (done) {
-        request({
-            uri: pot.resolve('accounts', '/apis/v/tokens'),
-            method: 'POST',
-            form: {
-                grant_type: 'password',
-                username: 'user0@serandives.com'
-            },
-            json: true,
-            auth: {
-                bearer: client.users[0].token
-            }
-        }, function (e, r, b) {
-            if (e) {
-                return done(e);
-            }
-            r.statusCode.should.equal(errors.unprocessableEntity().status);
-            should.exist(b);
-            should.exist(b.code);
-            should.exist(b.message);
-            b.code.should.equal(errors.unprocessableEntity().data.code);
-            done();
-        });
+  it('password grand type without password', function (done) {
+    request({
+      uri: pot.resolve('accounts', '/apis/v/tokens'),
+      method: 'POST',
+      headers: {
+        'X-Captcha': 'dummy'
+      },
+      form: {
+        grant_type: 'password',
+        username: 'user0@serandives.com'
+      },
+      json: true,
+      auth: {
+        bearer: client.users[0].token
+      }
+    }, function (e, r, b) {
+      if (e) {
+        return done(e);
+      }
+      r.statusCode.should.equal(errors.unprocessableEntity().status);
+      should.exist(b);
+      should.exist(b.code);
+      should.exist(b.message);
+      b.code.should.equal(errors.unprocessableEntity().data.code);
+      done();
     });
+  });
 
-    it('password grand type without client_id', function (done) {
-        request({
-            uri: pot.resolve('accounts', '/apis/v/tokens'),
-            method: 'POST',
-            form: {
-                grant_type: 'password',
-                username: 'user0@serandives.com',
-                password: '123456'
-            },
-            json: true
-        }, function (e, r, b) {
-            if (e) {
-                return done(e);
-            }
-            r.statusCode.should.equal(errors.unprocessableEntity().status);
-            should.exist(b);
-            should.exist(b.code);
-            should.exist(b.message);
-            b.code.should.equal(errors.unprocessableEntity().data.code);
-            done();
-        });
+  it('password grand type without client_id', function (done) {
+    request({
+      uri: pot.resolve('accounts', '/apis/v/tokens'),
+      method: 'POST',
+      headers: {
+        'X-Captcha': 'dummy'
+      },
+      form: {
+        grant_type: 'password',
+        username: 'user0@serandives.com',
+        password: '123456'
+      },
+      json: true
+    }, function (e, r, b) {
+      if (e) {
+        return done(e);
+      }
+      r.statusCode.should.equal(errors.unprocessableEntity().status);
+      should.exist(b);
+      should.exist(b.code);
+      should.exist(b.message);
+      b.code.should.equal(errors.unprocessableEntity().data.code);
+      done();
     });
+  });
 
-    it('password grand type with unauthorized password', function (done) {
-        request({
-            uri: pot.resolve('accounts', '/apis/v/tokens'),
-            method: 'POST',
-            form: {
-                client_id: client.serandivesId,
-                grant_type: 'password',
-                username: 'user0@serandives.com',
-                password: '123456'
-            },
-            json: true
-        }, function (e, r, b) {
-            if (e) {
-                return done(e);
-            }
-            r.statusCode.should.equal(errors.unauthorized().status);
-            should.exist(b);
-            should.exist(b.code);
-            should.exist(b.message);
-            b.code.should.equal(errors.unauthorized().data.code);
-            done();
-        });
+  it('password grand type with unauthorized password', function (done) {
+    request({
+      uri: pot.resolve('accounts', '/apis/v/tokens'),
+      method: 'POST',
+      headers: {
+        'X-Captcha': 'dummy'
+      },
+      form: {
+        client_id: client.serandivesId,
+        grant_type: 'password',
+        username: 'user0@serandives.com',
+        password: '123456',
+        redirect_uri: pot.resolve('accounts', '/auth')
+      },
+      json: true
+    }, function (e, r, b) {
+      if (e) {
+        return done(e);
+      }
+      r.statusCode.should.equal(errors.unauthorized().status);
+      should.exist(b);
+      should.exist(b.code);
+      should.exist(b.message);
+      b.code.should.equal(errors.unauthorized().data.code);
+      done();
     });
+  });
 
-    it('password grant type with valid password', function (done) {
-        request({
-            uri: pot.resolve('accounts', '/apis/v/tokens'),
-            method: 'POST',
-            form: {
-                client_id: client.serandivesId,
-                grant_type: 'password',
-                username: 'user0@serandives.com',
-                password: '1@2.Com'
-            },
-            json: true
-        }, function (e, r, b) {
-            if (e) {
-                return done(e);
-            }
-            r.statusCode.should.equal(200);
-            should.exist(b);
-            should.exist(b.id);
-            should.exist(b.access_token);
-            should.exist(b.refresh_token);
-            should.exist(b.expires_in);
-            b.expires_in.should.be.above(0)
-            request({
-                uri: pot.resolve('accounts', '/apis/v/tokens/' + b.id),
-                method: 'GET',
-                json: true,
-                auth: {
-                    bearer: b.access_token
-                }
-            }, function (e, r, b) {
-                if (e) {
-                    return done(e);
-                }
-                r.statusCode.should.equal(200);
-                should.exist(b);
-                should.exist(b.id);
-                should.exist(b.user);
-                should.exist(b.client);
-                should.exist(b.access);
-                should.exist(b.refresh);
-                should.exist(b.createdAt);
-                should.exist(b.accessible);
-                should.exist(b.refreshable);
-                should.exist(b.has);
-                done();
-            });
-        });
+  it('password grant type with valid password', function (done) {
+    request({
+      uri: pot.resolve('accounts', '/apis/v/tokens'),
+      method: 'POST',
+      headers: {
+        'X-Captcha': 'dummy'
+      },
+      form: {
+        client_id: client.serandivesId,
+        grant_type: 'password',
+        username: 'user0@serandives.com',
+        password: '1@2.Com',
+        redirect_uri: pot.resolve('accounts', '/auth')
+      },
+      json: true
+    }, function (e, r, b) {
+      if (e) {
+        return done(e);
+      }
+      r.statusCode.should.equal(200);
+      should.exist(b);
+      should.exist(b.id);
+      should.exist(b.access_token);
+      should.exist(b.refresh_token);
+      should.exist(b.expires_in);
+      b.expires_in.should.be.above(0)
+      request({
+        uri: pot.resolve('accounts', '/apis/v/tokens/' + b.id),
+        method: 'GET',
+        json: true,
+        auth: {
+          bearer: b.access_token
+        }
+      }, function (e, r, b) {
+        if (e) {
+          return done(e);
+        }
+        r.statusCode.should.equal(200);
+        should.exist(b);
+        should.exist(b.id);
+        should.exist(b.user);
+        should.exist(b.client);
+        should.exist(b.access);
+        should.exist(b.refresh);
+        should.exist(b.createdAt);
+        should.exist(b.accessible);
+        should.exist(b.refreshable);
+        should.exist(b.has);
+        done();
+      });
     });
+  });
 });
--- test/find.js
+++ test/find.js
@@ -6,129 +6,136 @@ var mongoose = require('mongoose');
 var errors = require('errors');
 
 describe('GET /clients', function () {
-    var serandivesId;
-    var user;
-    var accessToken;
-    var clientId;
-    before(function (done) {
+  var serandivesId;
+  var user;
+  var accessToken;
+  var clientId;
+  before(function (done) {
+    request({
+      uri: pot.resolve('accounts', '/apis/v/configs/boot'),
+      method: 'GET',
+      json: true
+    }, function (e, r, b) {
+      if (e) {
+        return done(e);
+      }
+      r.statusCode.should.equal(200);
+      should.exist(b);
+      should.exist(b.name);
+      b.name.should.equal('boot');
+      should.exist(b.value);
+      should.exist(b.value.clients);
+      should.exist(b.value.clients.serandives);
+      serandivesId = b.value.clients.serandives;
+      request({
+        uri: pot.resolve('accounts', '/apis/v/users'),
+        method: 'POST',
+        headers: {
+          'X-Captcha': 'dummy'
+        },
+        json: {
+          email: 'user@serandives.com',
+          password: '1@2.Com',
+        }
+      }, function (e, r, b) {
+        if (e) {
+          return done(e);
+        }
+        r.statusCode.should.equal(201);
+        should.exist(b);
+        should.exist(b.id);
+        should.exist(b.email);
+        b.email.should.equal('user@serandives.com');
+        user = b;
         request({
-            uri: pot.resolve('accounts', '/apis/v/configs/boot'),
-            method: 'GET',
-            json: true
+          uri: pot.resolve('accounts', '/apis/v/tokens'),
+          method: 'POST',
+          headers: {
+            'X-Captcha': 'dummy'
+          },
+          form: {
+            client_id: serandivesId,
+            grant_type: 'password',
+            username: 'user@serandives.com',
+            password: '1@2.Com',
+            redirect_uri: pot.resolve('accounts', '/auth')
+          },
+          json: true
         }, function (e, r, b) {
-            if (e) {
-                return done(e);
-            }
-            r.statusCode.should.equal(200);
-            should.exist(b);
-            should.exist(b.name);
-            b.name.should.equal('boot');
-            should.exist(b.value);
-            should.exist(b.value.clients);
-            should.exist(b.value.clients.serandives);
-            serandivesId = b.value.clients.serandives;
-            request({
-                uri: pot.resolve('accounts', '/apis/v/users'),
-                method: 'POST',
-                json: {
-                    email: 'user@serandives.com',
-                    password: '1@2.Com'
-                }
-            }, function (e, r, b) {
-                if (e) {
-                    return done(e);
-                }
-                r.statusCode.should.equal(201);
-                should.exist(b);
-                should.exist(b.id);
-                should.exist(b.email);
-                b.email.should.equal('user@serandives.com');
-                user = b;
-                request({
-                    uri: pot.resolve('accounts', '/apis/v/tokens'),
-                    method: 'POST',
-                    form: {
-                        client_id: serandivesId,
-                        grant_type: 'password',
-                        username: 'user@serandives.com',
-                        password: '1@2.Com'
-                    },
-                    json: true
-                }, function (e, r, b) {
-                    if (e) {
-                        return done(e);
-                    }
-                    r.statusCode.should.equal(200);
-                    should.exist(b.access_token);
-                    should.exist(b.refresh_token);
-                    accessToken = b.access_token;
-                    request({
-                        uri: pot.resolve('accounts', '/apis/v/clients'),
-                        method: 'POST',
-                        json: {
-                            name: 'serandives',
-                            to: ['http://test.serandives.com/dummy']
-                        },
-                        auth: {
-                            bearer: accessToken
-                        }
-                    }, function (e, r, b) {
-                        if (e) {
-                            return done(e);
-                        }
-                        r.statusCode.should.equal(201);
-                        should.exist(b);
-                        should.exist(b.id);
-                        should.exist(b.name);
-                        should.exist(b.to);
-                        b.name.should.equal('serandives');
-                        b.to.length.should.equal(1);
-                        b.to[0].should.equal('http://test.serandives.com/dummy');
-                        clientId = b.id;
-                        done();
-                    });
-                });
-            });
-        });
-    });
-
-    it('GET /clients/:id unauthorized', function (done) {
-        request({
-            uri: pot.resolve('accounts', '/apis/v/clients/' + clientId),
-            method: 'GET',
-            json: true
-        }, function (e, r, b) {
-            if (e) {
-                return done(e);
-            }
-            r.statusCode.should.equal(errors.unauthorized().status);
-            should.exist(b);
-            should.exist(b.code);
-            should.exist(b.message);
-            b.code.should.equal(errors.unauthorized().data.code);
-            done();
-        });
-    });
-
-    it('GET /users/:id', function (done) {
-        request({
-            uri: pot.resolve('accounts', '/apis/v/clients/' + clientId),
-            method: 'GET',
-            auth: {
-                bearer: accessToken
+          if (e) {
+            return done(e);
+          }
+          r.statusCode.should.equal(200);
+          should.exist(b.access_token);
+          should.exist(b.refresh_token);
+          accessToken = b.access_token;
+          request({
+            uri: pot.resolve('accounts', '/apis/v/clients'),
+            method: 'POST',
+            json: {
+              name: 'serandives',
+              to: ['http://test.serandives.com/dummy']
             },
-            json: true
-        }, function (e, r, b) {
-            if (e) {
-                return done(e);
+            auth: {
+              bearer: accessToken
             }
-            r.statusCode.should.equal(200);
+          }, function (e, r, b) {
+            if (e) {
+              return done(e);
+            }
+            r.statusCode.should.equal(201);
             should.exist(b);
             should.exist(b.id);
             should.exist(b.name);
-            b.id.should.equal(clientId);
+            should.exist(b.to);
             b.name.should.equal('serandives');
+            b.to.length.should.equal(1);
+            b.to[0].should.equal('http://test.serandives.com/dummy');
+            clientId = b.id;
             done();
+          });
         });
+      });
     });
+  });
+
+  it('GET /clients/:id unauthorized', function (done) {
+    request({
+      uri: pot.resolve('accounts', '/apis/v/clients/' + clientId),
+      method: 'GET',
+      json: true
+    }, function (e, r, b) {
+      if (e) {
+        return done(e);
+      }
+      r.statusCode.should.equal(errors.unauthorized().status);
+      should.exist(b);
+      should.exist(b.code);
+      should.exist(b.message);
+      b.code.should.equal(errors.unauthorized().data.code);
+      done();
+    });
+  });
+
+  it('GET /users/:id', function (done) {
+    request({
+      uri: pot.resolve('accounts', '/apis/v/clients/' + clientId),
+      method: 'GET',
+      auth: {
+        bearer: accessToken
+      },
+      json: true
+    }, function (e, r, b) {
+      if (e) {
+        return done(e);
+      }
+      r.statusCode.should.equal(200);
+      should.exist(b);
+      should.exist(b.id);
+      should.exist(b.name);
+      b.id.should.equal(clientId);
+      b.name.should.equal('serandives');
+      done();
+    });
+  });
 });
